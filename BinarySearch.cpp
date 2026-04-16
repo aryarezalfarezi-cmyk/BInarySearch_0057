23bad92cf479aa5b406bf2d606d97f29b94fcac2
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-04-16 18:02:13 +0700

    while pengulangan

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -4,3 +4,9 @@ using namespace std;
 int element[10];
 int nPanjang;
 int x;
+void input()
+{
+    while (true)
+    {
+        cout << "masukkan banyaknya elemen pada array (maksimal 10) :";
+        cin >> nPanjang;

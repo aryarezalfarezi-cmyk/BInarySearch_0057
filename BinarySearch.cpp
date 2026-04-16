91a72cdc9365749e8e25c2fbb8c064a4e4132b2b
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-04-16 18:05:13 +0700

    if elemen

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -36,3 +36,12 @@ void BubbleSortArray()
         for (int j = 0; j <= nPanjang - 1 - pass; j++)
         {
             if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+        pass++;
+    } while (pass <= nPanjang - 1);
+}
